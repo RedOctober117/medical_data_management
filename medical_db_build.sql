6937f1778d4bf25b1850db5cfa88ab9fde936f0c
RedOctober117 <dbcahilly3@gmail.com>
2023-09-30 11:33:42 -0400

    updated db

--- medical_db_build.sql
+++ medical_db_build.sql
@@ -20,76 +20,76 @@ CREATE TABLE medical_report_type (
 CREATE TABLE medical_report (
     report_id           INT             AUTO_INCREMENT,
     report_date         DATETIME        NOT NULL,
-    report_provider     INT             NOT NULL        DEFAULT '1',
-    report_type         INT             NOT NULL        DEFAULT '1',
+    report_provider     INT             NOT NULL,
+    report_type         INT             NOT NULL,
     PRIMARY KEY (report_id),
-    CONSTRAINT FOREIGN KEY (report_provider) REFERENCES medical_report_provider (provider_id) ON DELETE SET DEFAULT,
-    CONSTRAINT FOREIGN KEY (report_type) REFERENCES medical_report_type (report_type_id) ON DELETE SET DEFAULT
+    CONSTRAINT FOREIGN KEY (report_provider) REFERENCES medical_report_provider (provider_id),
+    CONSTRAINT FOREIGN KEY (report_type) REFERENCES medical_report_type (report_type_id)
 );
 
 CREATE TABLE provider_physical_address (
-    provider_id             INT                 NOT NULL        DEFAULT '1',
+    provider_id             INT                 NOT NULL,
     provider_street_address VARCHAR(400)        NOT NULL,
     PRIMARY KEY (provider_id, provider_street_address),
-    CONSTRAINT FOREIGN KEY (provider_id) REFERENCES medical_report_provider (provider_id) ON DELETE SET DEFAULT
+    CONSTRAINT FOREIGN KEY (provider_id) REFERENCES medical_report_provider (provider_id)
 );
 
 CREATE TABLE provider_contact_position_classification (
-    classification_id INT AUTO_INCREMENT,
-    classification_name VARCHAR(100) NOT NULL,
-    classification_description VARCHAR(400),
+    classification_id               INT             AUTO_INCREMENT,
+    classification_name             VARCHAR(100)    NOT NULL,
+    classification_description      VARCHAR(400),
     PRIMARY KEY (classification_id)
 );
 
 CREATE TABLE provider_contact_name (
-    provider_id                      INT             NOT NULL        DEFAULT '1',
+    provider_id                      INT             NOT NULL,
     provider_contact_id              INT             AUTO_INCREMENT,
-    provider_contact_classification  INT             NOT NULL        DEFAULT '1',
+    provider_contact_classification  INT             NOT NULL,
     provider_contact_last_name       VARCHAR(50)     NOT NULL,
     provider_contact_first_name      VARCHAR(50),
     PRIMARY KEY (provider_contact_id),
-    CONSTRAINT FOREIGN KEY (provider_id) REFERENCES medical_report_provider (provider_id) ON DELETE SET DEFAULT,
-    CONSTRAINT FOREIGN KEY (provider_contact_classification) REFERENCES provider_contact_position_classification (classification_id) ON DELETE SET DEFAULT
+    CONSTRAINT FOREIGN KEY (provider_id) REFERENCES medical_report_provider (provider_id),
+    CONSTRAINT FOREIGN KEY (provider_contact_classification) REFERENCES provider_contact_position_classification (classification_id)
 );
 
 CREATE TABLE provider_contact_email (
-    provider_id                         INT             NOT NULL        DEFAULT '1',
+    provider_id                         INT             NOT NULL,
     provider_contact_email_address      VARCHAR(200)    NOT NULL,
     PRIMARY KEY (provider_id, provider_contact_email_address),
-    CONSTRAINT FOREIGN KEY (provider_id) REFERENCES medical_report_provider (provider_id) ON DELETE SET DEFAULT
+    CONSTRAINT FOREIGN KEY (provider_id) REFERENCES medical_report_provider (provider_id)
 );
 
 CREATE TABLE provider_contact_phone (
-    provider_id                     INT             NOT NULL        DEFAULT '1',
+    provider_id                     INT             NOT NULL,
     provider_contact_phone_number   VARCHAR(200)    NOT NULL,
     PRIMARY KEY (provider_id, provider_contact_phone_number),
-    CONSTRAINT FOREIGN KEY (provider_id) REFERENCES medical_report_provider (provider_id) ON DELETE SET DEFAULT
+    CONSTRAINT FOREIGN KEY (provider_id) REFERENCES medical_report_provider (provider_id)
 );
 
-CREATE TABLE medical_report_value_units (
-    value_unit_id INT AUTO_INCREMENT,
-    value_unit_full_name VARCHAR(100) NOT NULL,
-    value_unit_description VARCHAR(400),
-    value_unit_shorthand VARCHAR(30) NOT NULL,
+CREATE TABLE medical_report_line_item_unit (
+    value_unit_id               INT             AUTO_INCREMENT,
+    value_unit_full_name        VARCHAR(100)    NOT NULL,
+    value_unit_description      VARCHAR(400),
+    value_unit_shorthand        VARCHAR(30)     NOT NULL,
     PRIMARY KEY (value_unit_id)
 );
 
-CREATE TABLE medical_report_value_type (
-    value_type_id INT AUTO_INCREMENT,
-    value_type_name VARCHAR(100) NOT NULL,
-    value_type_abbreviation VARCHAR(30) NOT NULL,
+CREATE TABLE medical_report_line_item_classification (
+    value_type_id               INT             AUTO_INCREMENT,
+    value_type_name             VARCHAR(100)    NOT NULL,
+    value_type_abbreviation     VARCHAR(30)     NOT NULL,
     PRIMARY KEY (value_type_id)
 );
 
-CREATE TABLE medical_report_data (
-    report_id INT NOT NULL,
-    value_type INT NOT NULL,
-    value_unit INT NOT NULL,
-    value_measurement DOUBLE NOT NULL,
+CREATE TABLE medical_report_line_item (
+    report_id           INT         NOT NULL,
+    value_type          INT         NOT NULL,
+    value_unit          INT         NOT NULL,
+    value_measurement   DOUBLE      NOT NULL,
     PRIMARY KEY (report_id, value_type),
-    CONSTRAINT FOREIGN KEY (report_id) REFERENCES medical_report (report_id),
-    CONSTRAINT FOREIGN KEY (value_type) REFERENCES medical_report_value_type (value_type_id) ON UPDATE CASCADE,
-    CONSTRAINT FOREIGN KEY (value_unit) REFERENCES medical_report_value_units (value_unit_id) ON UPDATE CASCADE
+    CONSTRAINT FOREIGN KEY (report_id)  REFERENCES medical_report (report_id),
+    CONSTRAINT FOREIGN KEY (value_type) REFERENCES medical_report_line_item_classification (value_type_id),
+    CONSTRAINT FOREIGN KEY (value_unit) REFERENCES medical_report_line_item_unit (value_unit_id)
 );
 
 -- TEST DATA
@@ -106,7 +106,7 @@ INSERT INTO medical_report_type (report_type_name) VALUES
     ("Complete Blood Cell Count with Automated Diff"),
     ("Complete Metabolic Panel");
 
-INSERT INTO medical_report_value_units (value_unit_full_name, value_unit_description, value_unit_shorthand) VALUES
+INSERT INTO medical_report_line_item_unit (value_unit_full_name, value_unit_description, value_unit_shorthand) VALUES
     ("Milligrams per Deciliter", "", "mg/dL"),
     ("Grams per Deciliter", "", "g/dL"),
     ("Milimoles per Liter", "", "mmol/L"),
@@ -117,7 +117,7 @@ INSERT INTO medical_report_value_units (value_unit_full_name, value_unit_descrip
     ("Picograms", "", "pg");
 
 
-INSERT INTO medical_report_value_type (value_type_name, value_type_abbreviation) VALUES
+INSERT INTO medical_report_line_item_classification (value_type_name, value_type_abbreviation) VALUES
     ("Albumin", "ALB"),
     ("Carbon Dioxide", "CO2"),
     ("Blood Urea Nitrogen", "BUN"),
@@ -167,13 +167,13 @@ INSERT INTO provider_contact_position_classification (classification_name) VALUE
 -- INSERT INTO medical_report (report_date, report_provider) VALUES
 --     ("2023-9-28", 1);
 
--- INSERT INTO medical_report_data (report_id, value_type, value_unit, value_measurement) VALUES
+-- INSERT INTO medical_report_line_item (report_id, value_type, value_unit, value_measurement) VALUES
 --     (1, 1, 1, 3.37);
 
 INSERT INTO medical_report (report_date, report_provider, report_type) VALUES
     ("2021-06-16", 2, 5);
 
-INSERT INTO medical_report_data (report_id, value_type, value_unit, value_measurement) VALUES
+INSERT INTO medical_report_line_item (report_id, value_type, value_unit, value_measurement) VALUES
     (1, 1, 1, 96);
 
 -- TEST QUERY
@@ -184,8 +184,9 @@ INSERT INTO medical_report_data (report_id, value_type, value_unit, value_measur
 --   mrd.value_measurement,
 --   mrvu.value_unit_shorthand  
 -- FROM 
--- 	medical_report_data mrd
---   	JOIN medical_report_value_units mrvu
+-- 	medical_report_line_item mrd
+--   	JOIN medical_report_line_item_unit mrvu
 --   		ON mrvu.value_unit_id = mrd.value_type
---    	JOIN medical_report_value_type mrvt
+--    	JOIN medical_report_line_item_classification mrvt
 --     	    ON mrvt.value_type_id = mrd.report_id
+
